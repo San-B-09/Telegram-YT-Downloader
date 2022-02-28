19fd4e39907773bd6192b43e6bbc9604f22e3a58
Sanket Bijawe <bijawesanket@gmail.com>
2022-02-28 17:44:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Telegram Youtube Content Downloader Bot
+This repo containts source code for building a telegram bot to donwload YouTube Content.
---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, abort
+import telegram
+from decouple import config
+
+API_KEY = config('API_KEY')
+USER_NAME = config('BOT_USER_NAME')
+URL = config('URL')
+
+bot = telegram.Bot(token=API_KEY)
+
+# start the flask app
+app = Flask(__name__)
+
+
+@app.route('/{}'.format(API_KEY), methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    if(update.message.text == None):
+        return 'ok'
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = (update.message.text.encode('utf-8').decode()).lower()
+
+    # the first time you chat with the bot AKA the welcoming message
+    if '/start' in text:
+        bot_welcome = """
+        Hi, I'm the base bot.\nWanna know more about my source code ??\nHop onto this link: https://github.com/San-B-09/Telegram-Bot-Base
+        """
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome,
+                        reply_to_message_id=msg_id)
+    elif "hi" in text:
+        bot_welcome = """
+        Welcome to Base Bot. 
+        """
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome,
+                        reply_to_message_id=msg_id)
+
+    elif "bye" in text:
+        bot_welcome = """
+        Byee!\nHave a productive time! 
+        """
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome,
+                        reply_to_message_id=msg_id)
+
+    return 'ok'
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+   s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=API_KEY))
+   if s:
+       return "webhook setup ok"
+   else:
+       return "webhook setup failed"
+
+
+@app.route('/')
+def index():
+   return '.'
+
+
+if __name__ == '__main__':
+   app.run(threaded=True)
